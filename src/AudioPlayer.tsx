5f88f0bb44775d9f4e039567162a9d933b70af6d
walk8243 <walk8243@gmail.com>
2026-04-06 01:10:16 +0900

    audioタグを作るだけ

---
+++ src/AudioPlayer.tsx
@@ -0,0 +1,13 @@
+import React, { forwardRef } from 'react';
+
+export interface AudioPlayerProps extends React.AudioHTMLAttributes<HTMLAudioElement> {
+  // カスタムプロパティが必要な場合はここに追加します
+}
+
+export const AudioPlayer = forwardRef<HTMLAudioElement, AudioPlayerProps>(
+  (props, ref) => {
+    return <audio ref={ref} {...props} />;
+  }
+);
+
+AudioPlayer.displayName = 'AudioPlayer';
