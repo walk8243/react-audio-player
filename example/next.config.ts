d02251d4da8488946d45dd9a3cf6bc3b237e966f
walk8243 <walk8243@gmail.com>
2026-04-27 17:40:42 +0900

    リアルタイムで変更をexampleで検証できるようにする

--- example/next.config.ts
+++ example/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: ["@walk8243/react-audio-player"],
 };
 
 export default nextConfig;
--- example/postcss.config.mjs
+++ example/postcss.config.mjs
@@ -1,6 +1,14 @@
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const config = {
   plugins: {
-    "@tailwindcss/postcss": {},
+    "@tailwindcss/postcss": {
+      base: __dirname,
+    },
   },
 };
 
